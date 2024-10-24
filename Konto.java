7d0d4c12e52149d3da64147b61f4276a9b159ef4
Denso666 <Denisolf@mx.de>
2024-10-24 07:44:57 +0200

    IW-1: create test cases and make them work

--- Konto.java
+++ Konto.java
@@ -1,14 +1,22 @@
 public class Konto {
-        private float kontostannd;
+        private float kontostand = 0;
 
-        public float getKontostand() {
-            return kontostannd;
+/*    public Konto(float kontostand) {
+        this.kontostand = kontostand;
+    }*/
+
+    public float getKontostand() {
+            return kontostand;
         }
         public void abheben(float summe) {
-            this.kontostannd = kontostannd - summe;
+            if (summe > kontostand && summe < 0) {
+                throw new IllegalArgumentException("Unvalid amount");
+            }
+            this.kontostand = kontostand - summe;
         }
         public void einzahlen(float summe) {
-            this.kontostannd = kontostannd + summe;
+            if (summe < 0) throw new IllegalArgumentException("Unvalid amount");
+            this.kontostand = kontostand + summe;
         }
     }
 
